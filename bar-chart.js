0398d521e96ea0f62c542e97f277e22c0ec0db45
S. Ritchie <sjritchie@MacBook-Pro.local>
2018-11-21 11:47:56 -0800

    The overall function with sample parameters. Nothing functioning yet.

---
+++ bar-chart.js
@@ -0,0 +1,10 @@
+function drawBarChart(data, options, element){
+  //create n divs (n = number of values given in data) -- these will be the bars -- class: "bar"
+
+}
+
+var sampleData = [1, 2, 3, 4, 5];
+var sampleOptions = {"width": "10", "height": "20px"};
+var sampleElement = $("#chart-container");
+
+drawBarChart(sampleData, sampleOptions, sampleElement);
