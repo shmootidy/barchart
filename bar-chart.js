c551746e2201541071702daa4810aed65f703356
shmootidy <shmooritchie@gmail.com>
2018-11-22 15:46:59 -0800

    in HTML: disabled some lines to allow JS file to create them from the DOM; in CSS: added grid-template-rows/columns to #chart-container; adjusted grid-row-end to 100 bc I don't understand why tutorial has it set larger than the rows the code creates; in JS: wrote a quick loop to generate the bars through the DOM; have abandoned the y-axis for now as I work to gain a better understanding of CSS Grids.

--- bar-chart.js
+++ bar-chart.js
@@ -1,9 +1,26 @@
 function drawBarChart(data, options, element){
-  //create n divs (n = number of values given in data) -- these will be the bars -- class: "bar"
+  let num = 0;
+  var chart = document.getElementById("chart-container");
+
+  function createBars(data){
+    for (var i = 0; i < data.length; i++){
+      let bar = document.createElement("div");
+      num++;
+      bar.setAttribute("class", "bar-" + num);
+      chart.appendChild(bar);
+    }
+  }
+
+  $("[class*='bar']").attr("grid-row-start", function(data){
+    //code here will extract names and values from data object (currently it's an array)
+  });
+
+  createBars(data);
 
 }
 
-var sampleData = [1, 2, 3, 4, 5];
+var sampleData = [1, 2, 3, 4];
+// var sampleData = {John: 50, Lucy: 12, Alexis: 24, Qing: 78};
 var sampleOptions = {"width": "10", "height": "20px"};
 var sampleElement = $("#chart-container");
 
