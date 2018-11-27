5ba71542a3795410b9b7940c0e9cb3afdf440848
shmootidy <shmooritchie@gmail.com>
2018-11-27 13:55:22 -0800

    Back to the drawing board... I found a sample JS(withjQ)/CSS/HTML bar chart that I'm using as a base for my assignment (here: https://www.jqueryscript.net/chart-graph/Minimalist-Bar-Chart-Plugin-With-jQuery-CSS-barChart.html). I will need to make substantial changes to this code: adding input fields, adjusting the size of various elements to respond to user input, addition of x- and y-axes labels, and likely several more I haven't thought of yet.

--- bar-chart.js
+++ bar-chart.js
@@ -1,27 +1,9 @@
-function drawBarChart(data, options, element){
-  let num = 0;
-  var chart = document.getElementById("chart-container");
+$(function(){
+  $("#bars li .bar").each(function(key, bar){
+    var percentage = $(this).data('percentage');
 
-  function createBars(data){
-    for (var i = 0; i < data.length; i++){
-      let bar = document.createElement("div");
-      num++;
-      bar.setAttribute("class", "bar-" + num);
-      chart.appendChild(bar);
-    }
-  }
-
-  $("[class*='bar']").attr("grid-row-start", function(data){
-    //code here will extract names and values from data object (currently it's an array)
-  });
-
-  createBars(data);
-
-}
-
-var sampleData = [1, 2, 3, 4];
-// var sampleData = {John: 50, Lucy: 12, Alexis: 24, Qing: 78};
-var sampleOptions = {"width": "10", "height": "20px"};
-var sampleElement = $("#chart-container");
-
-drawBarChart(sampleData, sampleOptions, sampleElement);
+    $(this).animate({
+      'height':percentage+'%'
+    }, 1000);
+  })
+})
