ac02e16ec804b323159fbf23f3c868313a57047d
shmootidy <shmooritchie@gmail.com>
2018-11-27 18:26:44 -0800

    starting to move the attributes from the CSS file to the JS file so that the chart can be adaptable

--- bar-chart.js
+++ bar-chart.js
@@ -1,9 +1,41 @@
 $(function(){
-  $("#bars li .bar").each(function(key, bar){
+
+  $("#chart #bars").attr({
+    "width": "600",
+    "height": "300",
+  });
+
+  $("#chart #bars li").attr("width", function(i, currentVal){
+
+  })
+
+
+
+
+  var data = [1, 2, 3, 4, 5];
+  var options;
+  // = {
+  //   chartHeight,
+  //   chartWidth,
+  //   barColors: {},
+  //   barSpacing,
+  //   xAxisLabel,
+  //   yAxisLabel,
+  //   chartTitle,
+  //   titleFontSize,
+  //   titleFontColor,
+  // };
+  var element;
+
+  function makeBarChart(){
+    $("#bars li .bar").each(function(key, bar){
     var percentage = $(this).data('percentage');
 
     $(this).animate({
       'height':percentage+'%'
     }, 1000);
   })
-})
+  }
+
+  makeBarChart(data, options, element);
+});
