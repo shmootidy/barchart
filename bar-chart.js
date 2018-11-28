821f56770046c8991981e19f4e71b2b126be7237
shmootidy <shmooritchie@gmail.com>
2018-11-27 22:44:27 -0800

    continuing the transfer of building from CSS/HTML to JS/jQ; meeting a hurdle in that #chart gets its size from the combined width of the columns, not the other way around; JS now builds the title and axes labels and height of the chart container (#bars)...actually, I think that last one probably doesn't work...

--- bar-chart.js
+++ bar-chart.js
@@ -1,41 +1,71 @@
 $(function(){
+  //things coming in from the user:
+  var data = [ //so far this is only used for its length
+    {label: "Option 1", dataPercentage: 56},
+    {label: "Option 2", dataPercentage: 24},
+    {label: "Option 3", dataPercentage: 87},
+    {label: "Option 4", dataPercentage: 12},
+    {label: "Option 5", dataPercentage: 75},
+    {label: "Option 6", dataPercentage: 49}
+    ];
+  var yAxisLabel = "Y-Axis Label";
+  var xAxisLabel = "X-Axis Label";
+  var customWidth = 600; // used to determine width of columns, available space between bars, not applied directly
+  var customHeight = 300;
+  var title = "My Bar Chart";
+  var yAxisTicks = 10;
+
+function generateYAxis(){
+  var topValue = 100;
+  for (var i = 0; i < yAxisTicks; i++){
+    $("#numbers").append("<li><span>" + topValue + "%</li></span>");
+    topValue = topValue - 10;
+  }
+  $("#numbers").append("<li><span></li></span>");
+}
+generateYAxis();
+
+
+  $("h1").append(title);
+  $("#chart #y-axis li span").append(yAxisLabel);
+  $("#chart #x-axis span").append(xAxisLabel);
 
   $("#chart #bars").attr({
-    "width": "600",
-    "height": "300",
+    "height": customHeight,
   });
 
-  $("#chart #bars li").attr("width", function(i, currentVal){
+  var numOfBars = data.length;
+  var columnWidth = customWidth / numOfBars; //100
+  var barSpacing = 30; //USER INPUT - must be smaller than columnWidth
+  var barWidth = columnWidth - barSpacing; //70
+  var barMargin = barSpacing / 2; //15
+  // $("#chart #bars li").attr({"width": columnWidth});
+  // $("#chart").load(location.href + " #chart>*", ""); //not working
 
-  })
 
 
-
-
-  var data = [1, 2, 3, 4, 5];
   var options;
   // = {
   //   chartHeight,
   //   chartWidth,
   //   barColors: {},
   //   barSpacing,
-  //   xAxisLabel,
-  //   yAxisLabel,
-  //   chartTitle,
   //   titleFontSize,
   //   titleFontColor,
   // };
   var element;
 
   function makeBarChart(){
+
     $("#bars li .bar").each(function(key, bar){
     var percentage = $(this).data('percentage');
 
     $(this).animate({
       'height':percentage+'%'
     }, 1000);
-  })
+  });
   }
 
   makeBarChart(data, options, element);
 });
+
