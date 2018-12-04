1d033d8fea970d5e1fbb57f208ed3ae608798978
shmootidy <shmooritchie@gmail.com>
2018-12-03 23:43:11 -0800

    cleaned up code to make everything neat; took functions out of the loop; condensed where appropriate; no linting errors anymore and the code still works :)

--- bar-chart.js
+++ bar-chart.js
@@ -1,12 +1,12 @@
 /*globals $:false */
+/* eslint multiline-comment-style: ["error", "separate-lines"] */
 $(function(){
-
 //                    ********** USER INPUT **********
   var data = [
     {item: "aadfadfasdf",
      amount: 0},
     {item: "bcxvcxvxcv",
-     amount: 46},
+     amount: 49},
     {item: "cjhjdjskska",
      amount: 34},
     {item: "dadfasdfasdfasdfasdffasd",
@@ -47,35 +47,23 @@ $(function(){
   var element;
 //                 ***** END OF USER INPUT *****
 
-// VARIABLES AHOY!
-var getBarLabelHeight;
+  function makeBarChart(data, options, element){
+  // VARIABLES AHOY!
+    var topAmountIntegers = getTopAmount().toString().length;
+    var topTest = getTopAmount()/Math.pow(10, topAmountIntegers);
+    var inBarLabelHeight = getOptions("inBarLabelHeight");
+    var barHeightAsPercent;
+    var yAxisTicks = getYAxisTicks();
 
-  var topAmountIntegers = getTopAmount().toString().length;
-  var topTest = getTopAmount()/Math.pow(10, topAmountIntegers);
-//crawl through options to return desired value
-  function getOptions(optionKey){
-    $.each(options, crawler);
-    function crawler(key, value){
-      if (optionKey == key){
-        optionKey = value;
-        return optionKey;
-      }
-      if (value !== null && typeof value === "object"){
-        $.each(value, crawler);
-      }
-    }
-    return optionKey;
-  }
-//set barsBox dimensions
-  var chartHeight = getOptions("chartHeight");
-  var chartWidth = getOptions("chartWidth");
-  $("#barsBox").attr({
-    "style": "height: " + chartHeight + "px; width: " + chartWidth + "px",
-  });
+  // SETTING THE ATTRIBUTES
+    // dimensions of the chart
+    var chartHeight = getOptions("chartHeight"); //not sure if I should move all variables to the top; there is no consensus, it seems
+    var chartWidth = getOptions("chartWidth");
+    $("#barsBox").attr({
+      "style": "height: " + chartHeight + "px; width: " + chartWidth + "px",
+    });
 
-//generate and style bars and x-axis labels
-  function generateBars(){
-    // bar spacing and size
+    // size of and space between bars
     var barSpacing = getOptions("barSpacing");
     $(".bars").attr({
       "style": "grid-column-gap: " + barSpacing + "px; "+
@@ -83,197 +71,191 @@ var getBarLabelHeight;
       "padding: 5px " + barSpacing + "px 0 " + barSpacing + "px", //not sure if the top (5px) should stay
     });
 
-    //x-labels (as grid area) spacing and size;
+    // size of and space between x-labels
     $("#values").attr({
       "style": "grid-template-columns: repeat(" + data.length + ", 1fr); "+
       "grid-column-gap: " + barSpacing + "px; "+
       "padding: 2px " + (barSpacing + 1) + "px; "+
       "width: " + chartWidth + "px",
     });
+    $("#numbers").attr({
+      "style": "grid-template-rows: repeat(" + yAxisTicks + ", 1fr)",
+    });
 
-    //loop through data to create bars
-    var num = 0;
-    for (var i = 0; i < data.length; i++){
-      var barHeight = Object.values(data[i])[1];
-
-
-      var barHeightInverse = 100 - getBarHeightAsPercent();
-
-      //create bars and give height
-      var barRadius = getOptions("barRadius");
-      num++;
-      var barClassName = "bar-" + num;
-      var bar = $("<div></div>").attr({
-        "class": barClassName,
-        "style": "grid-row-start: " + barHeightInverse + "; border-radius: " + barRadius + "px " + barRadius + "px 0 0",
-      });
-
-      //give bars in-bar labels
-      var inBarLabelHeight = getOptions("inBarLabelHeight");
-
-      getBarLabelHeight = function() {
-        if (inBarLabelHeight === 1){
-          if (getBarHeightAsPercent() < 90){
-            return "top: -20px";
-          } else {
-            return "top: 5px";
-          }
-        } else if (getBarHeightAsPercent() >= 10){
-          if (inBarLabelHeight === 2){
-            return "top: 5px";
-          } else if (inBarLabelHeight === 3) {
-            if (getBarHeightAsPercent() > 50){
-              return "top: 45%";
-            } else if (getBarHeightAsPercent() > 15){
-              return "top: 40%";
-            } else {
-              return "top: 30%";
-            }
-          } else if (inBarLabelHeight === 4) {
-            return "top: calc(100% - 20px)";
-          }
-        } else {
-          return "top: -20px";
-        }
-      };
-
-      function getBarHeightAsPercent(){
-        var barHeightAsPercent = Math.round(barHeight/Math.pow(10, topAmountIntegers) * 100);
-        if (chartZoomCheck() == 2){
-          barHeightAsPercent = barHeightAsPercent * 2;
-        }
-        return barHeightAsPercent;
-      }
-      var inBarLabel = $("<span></span>").text(Object.values(data[i])[1]);
-      inBarLabelHeight = getBarLabelHeight();
-      inBarLabel.attr({
-        "style": inBarLabelHeight,
-      });
-      inBarLabel.appendTo(bar);
-
-      //append bars
-      bar.appendTo(".bars");
-
-      //x-axis labels
-      var xStyle = getXStyle();
-
-      //alternate colours of x-axis labels to match their bars
-      function getXStyle(){
-        var barColor;
-        if (i % 2 === 0){
-           barColor = getOptions("odd");
-        } else {
-           barColor = getOptions("even");
-        }
-        if (getOptions("xStyle") === 1){
-          return "border: 2px solid; border-color: " + barColor;
-        } else if (getOptions("xStyle") === 2){
-          return "background-color: " + barColor;
-        } else if (getOptions("xStyle") === 3){
-          return "color: " + barColor;
-        }
-      }
-      //create and append x-axis labels
-      var xBarLabel = $("<div></div>").attr({
-        "class": "x-label",
-        "style": "padding: 5px; border-radius: 0 0 " + barRadius + "px " + barRadius + "px; " + xStyle,
-      }).text(Object.values(data[i])[0]);
-      //append x-labels
-      xBarLabel.appendTo("#values");
-    } //the end of the loop
-  } //end of generateBar() function
-  generateBars();
-
-
-// fill the chart title with custom text of a custom size and custom colour
-  function fillTitle(){
+  // FILLING STATIC ELEMENTS
+    // chart title
     var title = getOptions("title");
     var fontSize = getOptions("fontSize");
     var fontColor = getOptions("fontColor");
     $("h1").attr({"style": "font-size: " + fontSize + "px; "+
       "color: " + fontColor}).append(title);
-  }
-  fillTitle();
 
-// fill y-axis title
-  var yAxisTitle = getOptions("yAxisTitle");
-  $("<div id='y-axis'>" + yAxisTitle + "</div>").attr({
-// "style" : "transform: rotate(270deg)",
-}).prependTo($("#y-title"));
-
-// generate and fill y-axis and labels
-  var yAxisTicks = getYAxisTicks();
-  function getYAxisTicks(){
-     if (getOptions("yAxisTicks") === 1){
-      yAxisTicks = 10;
-     } else if (getOptions("yAxisTicks") === 2) {
-      yAxisTicks = 5;
-     }
-     return yAxisTicks;
-  }
-
-  function getTopValue(){
-    var topValue = Math.ceil(getTopAmount()/Math.pow(10, topAmountIntegers)) * Math.pow(10, topAmountIntegers);
-    if (Math.round(topTest) == 0){
-      topValue = topValue / 2;
-    }
-    return topValue;
-  }
-
-  function getTopAmount(){
-    var amountArray = [];
-    for (var i = 0; i < data.length; i++){
-      amountArray.push(Object.values(data[i])[1]);
-      amountArray.sort(sortAmounts); // got an explanation (and different way of expressing) the same function which helps me understand (though I'm still not fully clear what's happening)
-    }
-    var topAmount = amountArray[0];
-    return topAmount;
-  }
-  //sorting function for the input amount array (to find top amount)
-  function sortAmounts (a, b) {
-    return a > b ? -1 : b > a ? 1 : 0;
-  }
-
-  function generateYAxis(){
-    $("#numbers").attr({
-      "style": "grid-template-rows: repeat(" + yAxisTicks + ", 1fr)",
-    });
-    var num = getTopValue();
-    for (var i = 0; i < yAxisTicks; i++){
-      $("<div class='numbers'>" + num + "</div>").appendTo("#numbers");
-      num = num - (Math.pow(10, (topAmountIntegers - 1)) * (10 / yAxisTicks)) / chartZoomCheck();
-    }
-  }
-  generateYAxis();
-
-  function chartZoomCheck(){
-    var chartZoom = 1;
-    if (Math.round(topTest) == 0){
-      chartZoom = 2;
-    }
-    return chartZoom;
-  }
-
-  function generateTicks(){
+    // ticks grid
     $("#ticks").attr({
       "style": "grid-template-rows: repeat(" + yAxisTicks + ", 1fr)",
     });
-    for (var i = 0; i < yAxisTicks; i++){
+
+    // y-axis title
+    var yAxisTitle = getOptions("yAxisTitle");
+    $("<div id='y-axis'>" + yAxisTitle + "</div>").prependTo("#y-title");
+
+    // x-axis title
+    var xAxisTitle = getOptions("X-Axis Title");
+    $("#chart #x-axis span").append(xAxisTitle);
+
+  // LOOP 1 of 2 -- create, style and append the bars, in-bar labels and x-labels
+    for (var i = 0; i < data.length; i++){
+      // THE BARS
+      var barHeight = Object.values(data[i])[1];
+      var barHeightInverse = 100 - getBarHeightAsPercent(barHeight);
+      var barRadius = getOptions("barRadius");
+      var barClassName = "bar-";
+      var bar = $("<div></div>").attr({
+        "class": barClassName,
+        "style": "grid-row-start: " + barHeightInverse + "; border-radius: " + barRadius + "px " + barRadius + "px 0 0",
+      });
+      // IN-BAR LABELS
+      var inBarLabel = $("<span></span>").text(Object.values(data[i])[1]);
+      var fixedInBarLabelHeight = getBarLabelHeight(barHeight);
+      inBarLabel.attr({
+        "style": fixedInBarLabelHeight,
+      });
+      // X-LABELS
+      var xStyle = getXStyle(i);
+      var xBarLabel = $("<div></div>").attr({
+        "class": "x-label",
+        "style": "padding: 5px; border-radius: 0 0 " + barRadius + "px " + barRadius + "px; " + xStyle,
+      }).text(Object.values(data[i])[0]);
+      // append each element
+      inBarLabel.appendTo(bar);
+      xBarLabel.appendTo("#values");
+      bar.appendTo(".bars");
+    }
+
+  // LOOP 2 of 2 -- create and append the y-labels and ticks
+    var yLabel = getTopValue();
+    for (var y = 0; y < yAxisTicks; y++){
+      // Y-LABELS
+      $("<div class='numbers'>" + yLabel + "</div>").appendTo("#numbers");
+      yLabel = yLabel - (Math.pow(10, (topAmountIntegers - 1)) * (10 / yAxisTicks)) / chartZoomCheck();
+      // TICKS
       $("<div class='ticks'>" + "" + "</div>").appendTo("#ticks");
     }
+
+  //THE FUNCTIONS
+    // return the desired value by key
+    function getOptions(optionKey){
+      $.each(options, crawler);
+      function crawler(key, value){
+        if (optionKey == key){
+          optionKey = value;
+          return optionKey;
+        }
+        if (value !== null && typeof value === "object"){
+          $.each(value, crawler);
+        }
+      }
+      return optionKey;
+    }
+
+    // return bar height as percentage of y-axis height
+    function getBarHeightAsPercent(barHeight){
+      barHeightAsPercent = Math.round(barHeight/Math.pow(10, topAmountIntegers) * 100);
+      if (chartZoomCheck() == 2){
+        barHeightAsPercent = barHeightAsPercent * 2;
+      }
+      return barHeightAsPercent;
+    }
+
+    // return style for selected height of in-bar labels
+    function getBarLabelHeight(barHeight) {
+      barHeightAsPercent = getBarHeightAsPercent(barHeight);
+      if (inBarLabelHeight === 1){
+        if (barHeightAsPercent < 90){
+          return "top: -20px";
+        } else {
+          return "top: 5px";
+        }
+      } else if (barHeightAsPercent >= 10){
+        if (inBarLabelHeight === 2){
+          return "top: 5px";
+        } else if (inBarLabelHeight === 3) {
+          if (barHeightAsPercent > 50){
+            return "top: 45%";
+          } else if (barHeightAsPercent > 15){
+            return "top: 40%";
+          } else {
+            return "top: 30%";
+          }
+        } else if (inBarLabelHeight === 4) {
+          return "top: calc(100% - 20px)";
+        }
+      } else {
+        return "top: -20px";
+      }
+    }
+
+    // return odd/even styles for x-axis labels
+    function getXStyle(i){
+      var barColor;
+      if (i % 2 === 0){
+         barColor = getOptions("odd");
+      } else {
+         barColor = getOptions("even");
+      }
+      if (getOptions("xStyle") === 1){
+        return "border: 2px solid; border-color: " + barColor;
+      } else if (getOptions("xStyle") === 2){
+        return "background-color: " + barColor;
+      } else if (getOptions("xStyle") === 3){
+        return "color: " + barColor;
+      }
+    }
+
+    // generate and fill y-axis and labels
+    function getYAxisTicks(){
+       if (getOptions("yAxisTicks") === 1){
+        yAxisTicks = 10;
+       } else if (getOptions("yAxisTicks") === 2) {
+        yAxisTicks = 5;
+       }
+       return yAxisTicks;
+    }
+
+    // generate an appropriate highest value for the y-axis
+    function getTopValue(){
+      var topValue = Math.ceil(getTopAmount()/Math.pow(10, topAmountIntegers)) * Math.pow(10, topAmountIntegers);
+      if (Math.round(topTest) == 0){
+        topValue = topValue / 2;
+      }
+      return topValue;
+    }
+
+    // return value of highest data amount
+    function getTopAmount(){
+      var amountArray = [];
+      for (var i = 0; i < data.length; i++){
+        amountArray.push(Object.values(data[i])[1]);
+        amountArray.sort(sortAmounts);
+      }
+      var topAmount = amountArray[0];
+      return topAmount;
+    }
+
+    // sorts data amounts array from highest to lowest (as numbers, not strings)
+    function sortAmounts (a, b) {
+      return a > b ? -1 : b > a ? 1 : 0;
+    }
+
+    // checks if y-axis is full or half length
+    function chartZoomCheck(){
+      var chartZoom = 1;
+      if (Math.round(topTest) == 0){
+        chartZoom = 2;
+      }
+      return chartZoom;
+    }
   }
-  generateTicks();
-
-//fill x-axis title
-  var xAxisTitle = getOptions("X-Axis Title");
-  $("#chart #x-axis span").append(xAxisTitle);
-
-
-//aiming to fit everything in here
   makeBarChart(data, options, element);
-  function makeBarChart(data, options, element){
-
-  }
-
 });
 
