e6ab455d4fb7aa436050c49c214efbbcee23f162
shmootidy <shmooritchie@gmail.com>
2018-12-19 11:30:19 -0800

    Whoops! Still learning how git works. All the notes of the previous commit actually apply to THIS commit: calling the bar chart with the proper function; adding the random class to avoid erroneous appending; dynamic background-colors for the bars; rearranging blocks within the JS file.

--- bar-chart.js
+++ bar-chart.js
@@ -1,51 +1,6 @@
 /*globals $:false */
 /* eslint multiline-comment-style: ["error", "separate-lines"] */
 $(function(){
-//                    ********** USER INPUT **********
-  var data = [
-    {item: "aadfadfasdf",
-     amount: 0},
-    {item: "bcxvcxvxcv",
-     amount: 49},
-    {item: "cjhjdjskska",
-     amount: 34},
-    {item: "dadfasdfasdfasdfasdffasd",
-     amount: 26},
-    {item: "ertuwisdhfsdfga",
-     amount: 9},
-    {item: "fasdfasdfashaj",
-     amount: 31},
-  ];
-  var options = {
-    chart: {
-      chartWidth: 600,
-      chartHeight: 300,
-      chartTitle: {
-        title: "My Bar Chart",
-        fontSize: 36,
-        fontColor: "#eee",
-      },
-    },
-    bars: {
-      barSpacing: 10,
-      barColors: {
-        odd: "#1a77ff",
-        even: "#e2638e",
-      },
-      inBarLabelHeight: 1, // four options: 1 - hover; 2 - top; 3- middle; 4 - bottom
-      barRadius: 5,
-    },
-    xAxis: {
-      xStyle: 1, // three options: 1 - outline; 2 - background; 3 - text color
-    },
-    yAxis: {
-      xAxisTitle: "X-Axis Title",
-      yAxisTitle: "Y-Axis Title",
-      yAxisTicks: 2, //two option: 1 - 10 ticks; 2 - 5 ticks
-    }
-  };
-  var element = "body";
-//                 ***** END OF USER INPUT *****
 
   function makeBarChart(data, options, element){
   // VARIABLES AHOY!
@@ -54,10 +9,16 @@ $(function(){
     var inBarLabelHeight = getOptions("inBarLabelHeight");
     var barHeightAsPercent;
     var yAxisTicks = getYAxisTicks();
+    var randomClass = getRandomClass();
+
+    function getRandomClass(){
+      randomClass = Math.random().toString(36).replace(/[^a-z]+/g, "").substr(0, 5);
+      return randomClass;
+    }
 
   // CREATE THE HTML FRAMEWORK
-    $("<div id='chart'>"+
-    "<h1><!-- chart title goes here --></h1>"+
+   var htmlFrame = $("<div id='chart'>"+
+    "<h1 class='"+ randomClass + "'><!-- chart title goes here --></h1>"+
     "<div id='y-title'>"+
       "<!-- y title goes here -->"+
     "</div>"+
@@ -81,10 +42,15 @@ $(function(){
         "<!-- x-axis labels go here -->"+
       "</div>"+
     "</div>"+
-    "<div id='x-axis'><span>"+
+    "<div id='x-axis'>"+
       "<!-- x-axis title goes here -->"+
-    "</span></div>"+
-  "</div>").prependTo(element);
+    "</div>"+
+    "</div>");
+
+   var chartBox = $("<div class='" + randomClass + "'></div>");
+   htmlFrame.prependTo(chartBox);
+   chartBox.prependTo(element);
+
 
   // SET THE ATTRIBUTES
     // dimensions of the chart
@@ -96,7 +62,7 @@ $(function(){
 
     // number of and space between bars
     var barSpacing = getOptions("barSpacing");
-    $(".bars").attr({
+    $("." + randomClass + " .bars").attr({
       "style": "grid-column-gap: " + barSpacing + "px; "+
       "grid-template-columns: repeat(" + data.length + ", 1fr); " +
       "padding: 5px " + barSpacing + "px 0 " + barSpacing + "px", //not sure if the top (5px) should stay
@@ -120,7 +86,7 @@ $(function(){
     var title = getOptions("title");
     var fontSize = getOptions("fontSize");
     var fontColor = getOptions("fontColor");
-    $("h1").attr({"style": "font-size: " + fontSize + "px; "+
+    $("." + randomClass + " h1").attr({"style": "font-size: " + fontSize + "px; "+
       "color: " + fontColor}).append(title);
 
     // ticks grid
@@ -133,40 +99,46 @@ $(function(){
     $("<div id='y-axis'>" + yAxisTitle + "</div>").prependTo("#y-title");
 
     // x-axis title
-    var xAxisTitle = getOptions("X-Axis Title");
-    $("#chart #x-axis span").append(xAxisTitle);
+    var xAxisTitle = getOptions("xAxisTitle");
+    $("<span>" + xAxisTitle + "</span>").prependTo("#x-axis");
 
   // LOOP 1 of 2 -- create, style and append the bars, in-bar labels and x-labels
-    for (var i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++){
       // THE BARS
+      var barStyle = getBarStyle(i);
       var barHeight = Object.values(data[i])[1];
       var barHeightInverse = 100 - getBarHeightAsPercent(barHeight);
       var barRadius = getOptions("barRadius");
       var bar = $("<div></div>").attr({
-        "class": "bar-",
-        "style": "grid-row-start: " + barHeightInverse + "; border-radius: " + barRadius + "px " + barRadius + "px 0 0",
+        "class": "bar- " + randomClass,
+        "style": "grid-row-start: " + barHeightInverse +
+         "; border-radius: " + barRadius + "px " + barRadius + "px 0 0; " +
+         barStyle,
       });
+
       // IN-BAR LABELS
       var inBarLabel = $("<span></span>").text(Object.values(data[i])[1]);
       var correctedInBarLabelHeight = getBarLabelHeight(barHeight);
       inBarLabel.attr({
         "style": correctedInBarLabelHeight,
       });
+
       // X-LABELS
       var xStyle = getXStyle(i);
       var xBarLabel = $("<div></div>").attr({
         "class": "x-label",
         "style": "padding: 5px; border-radius: 0 0 " + barRadius + "px " + barRadius + "px; " + xStyle,
       }).text(Object.values(data[i])[0]);
+
       // append each element
       inBarLabel.appendTo(bar);
       xBarLabel.appendTo("#values");
-      bar.appendTo(".bars");
+      bar.appendTo("." + randomClass + " .bars");
     }
 
   // LOOP 2 of 2 -- create and append the y-labels and ticks
     var yLabel = getTopValue();
-    for (var y = 0; y <= yAxisTicks; y++){
+    for (let y = 0; y < yAxisTicks; y++){
       // Y-LABELS
       $("<div class='numbers'>" + yLabel + "</div>").appendTo("#numbers");
       yLabel = yLabel - (Math.pow(10, (topAmountIntegers - 1)) * (10 / yAxisTicks)) / chartZoomCheck();
@@ -202,16 +174,16 @@ $(function(){
     // return style for selected height of in-bar labels
     function getBarLabelHeight(barHeight) {
       barHeightAsPercent = getBarHeightAsPercent(barHeight);
-      if (inBarLabelHeight === 1){
+      if (inBarLabelHeight === 1){ // hover
         if (barHeightAsPercent < 90){
           return "top: -20px";
         } else {
           return "top: 5px";
         }
       } else if (barHeightAsPercent >= 10){
-        if (inBarLabelHeight === 2){
+        if (inBarLabelHeight === 2){ // top
           return "top: 5px";
-        } else if (inBarLabelHeight === 3) {
+        } else if (inBarLabelHeight === 3) { // middle
           if (barHeightAsPercent > 50){
             return "top: 45%";
           } else if (barHeightAsPercent > 15){
@@ -219,28 +191,38 @@ $(function(){
           } else {
             return "top: 30%";
           }
-        } else if (inBarLabelHeight === 4) {
-          return "top: calc(100% - 20px)";
+        } else if (inBarLabelHeight === 4) { // bottom
+          return "top: calc(100% - 30px)";
         }
       } else {
         return "top: -20px";
       }
     }
 
-    // return odd/even styles for x-axis labels
-    function getXStyle(i){
+    //return odd/even styles of bars
+    function getBarStyle(i){
       var barColor;
       if (i % 2 === 0){
-         barColor = getOptions("odd");
+        barColor = getOptions("odd");
       } else {
-         barColor = getOptions("even");
+        barColor = getOptions("even");
+      }
+      return "background-color: " + barColor;
+    }
+    // return odd/even styles for x-axis labels
+    function getXStyle(i){
+      var xColor;
+      if (i % 2 === 0){
+         xColor = getOptions("odd");
+      } else {
+         xColor = getOptions("even");
       }
       if (getOptions("xStyle") === 1){
-        return "border: 2px solid; border-color: " + barColor;
+        return "border: 2px solid; border-color: " + xColor;
       } else if (getOptions("xStyle") === 2){
-        return "background-color: " + barColor;
+        return "background-color: " + xColor;
       } else if (getOptions("xStyle") === 3){
-        return "color: " + barColor;
+        return "color: " + xColor;
       }
     }
 
@@ -288,6 +270,156 @@ $(function(){
       return chartZoom;
     }
   }
+  var data = [
+    {item: "aadfadfasdf",
+     amount: 0},
+    {item: "bcxvcxvxcv",
+     amount: 49},
+    {item: "cjhjdjskska",
+     amount: 34},
+    {item: "dadfasdfasdfasdfasdffasd",
+     amount: 26},
+    {item: "ertuwisdhfsdfga",
+     amount: 9},
+    {item: "fasdfasdfashaj",
+     amount: 31},
+  ];
+  var options = {
+    chart: {
+      chartWidth: 600,
+      chartHeight: 300,
+      chartTitle: {
+        title: "Le Chart",
+        fontSize: 36,
+        fontColor: "#eee",
+      },
+    },
+    bars: {
+      barSpacing: 10,
+      barColors: {
+        odd: "#ffa676",
+        even: "#9bd2ad",
+      },
+      inBarLabelHeight: 1, // four options: 1 - hover; 2 - top; 3- middle; 4 - bottom
+      barRadius: 3,
+    },
+    xAxis: {
+      xStyle: 1, // three options: 1 - outline; 2 - background; 3 - text color
+    },
+    yAxis: {
+      xAxisTitle: "X Marks It",
+      yAxisTitle: "Y, oh Y",
+      yAxisTicks: 2, //two option: 1 - 10 ticks; 2 - 5 ticks
+    }
+  };
+  var element = "body";
+ var data1 = [
+    {item: "qqqqq",
+     amount: 23},
+    {item: "wwwwww",
+     amount: 43},
+    {item: "eeeeee",
+     amount: 71},
+    {item: "rrrrrrr",
+     amount: 63},
+  ];
+  var options1 = {
+    chart: {
+      chartWidth: 600,
+      chartHeight: 300,
+      chartTitle: {
+        title: "My Bar Chart",
+        fontSize: 36,
+        fontColor: "#eee",
+      },
+    },
+    bars: {
+      barSpacing: 10,
+      barColors: {
+        odd: "#1a77ff",
+        even: "#e2638e",
+      },
+      inBarLabelHeight: 4, // four options: 1 - hover; 2 - top; 3- middle; 4 - bottom
+      barRadius: 7,
+    },
+    xAxis: {
+      xStyle: 3, // three options: 1 - outline; 2 - background; 3 - text color
+    },
+    yAxis: {
+      xAxisTitle: "XXX",
+      yAxisTitle: "YYY",
+      yAxisTicks: 2, //two option: 1 - 10 ticks; 2 - 5 ticks
+    }
+  };
+  var element1 = "body";
+
+  var data2 = [
+    {item: "parrot",
+     amount: 2344},
+    {item: "donkey",
+     amount: 7674},
+    {item: "jackalope",
+     amount: 1735},
+    {item: "penguin",
+     amount: 3457},
+    {item: "platypus",
+     amount: 8284},
+     {item: "blob",
+     amount: 8444},
+  ];
+  var options2 = {
+    chart: {
+      chartWidth: 150,
+      chartHeight: 200,
+      chartTitle: {
+        title: "Animals",
+        fontSize: 36,
+        fontColor: "#eee",
+      },
+    },
+    bars: {
+      barSpacing: 10,
+      barColors: {
+        odd: "#fdff00",
+        even: "#2fff00",
+      },
+      inBarLabelHeight: 3, // four options: 1 - hover; 2 - top; 3- middle; 4 - bottom
+      barRadius: 0,
+    },
+    xAxis: {
+      xStyle: 2, // three options: 1 - outline; 2 - background; 3 - text color
+    },
+    yAxis: {
+      xAxisTitle: "X-Axis Title",
+      yAxisTitle: "Y-Axis Title",
+      yAxisTicks: 1, //two option: 1 - 10 ticks; 2 - 5 ticks
+    }
+  };
+  var element2 = "body";
+
   makeBarChart(data, options, element);
+  makeBarChart(data1, options1, element1);
+  makeBarChart(data2, options2, element2);
+
+
+ // function dataMaker(){
+  //   var stackedData = [];
+  //   var itemA = ["Joey", "Chloe", "Moe"];
+  //   var itemB = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+  //   for (var i = 0; i < itemA.length; i++){
+  //     stackedData[i] = [];
+  //     for (var j = 0; j < itemB.length; j++){
+  //       stackedData[i][j] = [];
+  //       stackedData[i][j][1] = Math.floor(Math.random() * 100);
+  //       stackedData[i][j][0] = itemB[j];
+  //     }
+  //     stackedData[i].unshift(itemA[i]);
+  //   }
+
+  //   return stackedData;
+  // }
+
+  // makeBarChart(dataMaker(), options, element);
 });
 
